057635be64793f0db413416b2563e6d351303a75
joaohl19 <108703483+joaohl19@users.noreply.github.com>
2023-08-14 00:22:03 -0300

    Update client.py

--- client.py
+++ client.py
@@ -4,25 +4,19 @@ import time
 
 s = cn.connect(2037)
 
-print(s)
-
-def choose_action(jogadas):
-    action = random.randint(0, 2)
-    return action
-
 def initialize_table(q_table):
     for i in range(96):
-        #print("\n")
         linha = []
         for j in range(3):
-            #print(f'{0}', end=" ")
             linha.append(0)
 
         q_table.append(linha)
 
 def q_table_update(q_value, coluna, alfa, gama, reward, max_value):
-    # Updates q_table
+    # Atualiza q_table
     q_value = (1 - alfa) * q_value + (alfa * (reward + (gama * max_value)))
+    return q_value
+
 
 # As linhas de 0 <= index <= 23 indicam as plataformas da 0 até a 23 com a direção do personagem NORTH
 
@@ -44,40 +38,52 @@ alfa = 0.2 # Taxa de aprendizagem
 gama = 0.5 # Taxa de desconto
 
 # Modelando o estado inicial
-current_platform = 0 # 0
-current_direction = 0 # North
-current_state = current_platform + (24 * current_direction) # Multiplicação da direção pelo número da plataforma para mapear a linha certa de acordo com a direção
 
-while True:
+current_state = 0 # Platform = 0  /  Direction = North
 
-    action_idx = choose_action(jogadas)
+# O usuário escolhe o tipo de ação do personagem
+action_type = int(input())
+i = 1000
+while i > 0:
+
+    # Escolha de ação inteligente
+    if action_type == 0:
+        action_value = q_table[current_state][0]
+        action_idx = 0
+        for j in range(3):
+            if q_table[current_state][j] > action_value:
+                    action_value = q_table[current_state][j]
+                    action_idx = j 
+
+    # Escolha de ação burra
+    else:      
+        action_idx = random.randint(0, 2)
+
     action = jogadas[action_idx]
 
     current_value = q_table[current_state][action_idx] # Atribui ao CURRENT VALUE o valor da tabela para o estado CURRENT STATE com a ação escolhida
     
     state_str, reward = cn.get_state_reward(s, action)  # Recebe o estado e a recompensa resultantes da ação
 
-    state_str = str(state_str)
-    platform = int(state_str[2:7], 2) # Converting platform number to integer
-    direction = int(state_str[7:9], 2) # North = 0 / East = 1 / South = 2 / West= 3
-
-    print(action)
-    print(platform)
-    print(direction)
-
-    time.sleep(30)
+    platform = int(str(state_str)[2:7], 2) # Converting platform number to integer
+    direction = int(str(state_str)[7:9], 2) # North = 0 / East = 1 / South = 2 / West= 3
     
     state = platform + (direction * 24) # Nova variável STATE como inteiro para ser usada nos acessos à q_table
 
-    max_value = max(q_table[state][0], q_table[state][1], q_table[state][2]) # Defiine o maior reforço futuro possível
+    max_value = max(q_table[state][0], q_table[state][1], q_table[state][2]) # Define o maior reforço futuro possível
 
-    q_table_update(current_value, action_idx, alfa, gama, reward, max_value) # Atualiza a q_table
+    q_value = q_table_update(current_value, action_idx, alfa, gama, reward, max_value) # Recebe o valor atualizado da q_table
+
+    q_table[current_state][action_idx] = q_value
 
     current_state = state # Atualiza o current state antes da próxima jogada
-    
-  
 
+    i = i - 1
 
-
-
+# Escreve a tabela em um arquivo .txt
+with open('resultado.txt', 'w') as f:
+    for i in range(96):
+        for j in range(3):
+            f.write((str(q_table[i][j]) + " "))
+        f.write("\n")
 
